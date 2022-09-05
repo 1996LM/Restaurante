f89a0743a9573646e4a44d1ff7de0e3d79b940ae
1996LM <lmiguel.hoyos@gmail.com>
2022-09-04 21:52:34 -0500

    uno mas

---
+++ restaurante_sistem/empleados.py
@@ -0,0 +1,5 @@
+from univercity_sistem.database_service.generic_database import GenericDataBase
+
+class EmpleadosDatabase(GenericDataBase):
+    def __init__(self)->None:
+        super().__init__(database_name="teachers")
